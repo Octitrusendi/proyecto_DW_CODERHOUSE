3659566cff2bdf32f3519b1daa21c792ccc03329
octitrusendi <octaviotrusendi@gmail.com>
2021-08-14 01:33:44 -0300

    credenciales

--- views/resultados.php
+++ views/resultados.php
@@ -84,7 +84,7 @@ try {
     $mail->Host       = 'smtp.hostinger.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
     $mail->Username   = 'info@laboratorio.net.ar';                     //SMTP username
-    $mail->Password   = 'PASSS';                               //SMTP password
+    $mail->Password   = '32778535Labo';                               //SMTP password
     $mail->SMTPSecure = 'ssl';            //Enable implicit TLS encryption
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
     
