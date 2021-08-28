84e7486192fa5e112dd50acbe16df8e9a9568c61
octitrusendi <octaviotrusendi@gmail.com>
2021-08-28 14:37:25 -0300

    proyecto final

--- js/scrip.js
+++ js/scrip.js
@@ -55,8 +55,8 @@ window.onscroll = function(){
         ],
     
         stringsElement: '#cadenas-texto', // ID del elemento que contiene cadenas de texto a mostrar.
-        typeSpeed: 75, // Velocidad en mlisegundos para poner una letra,
-        startDelay: 300, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
+        typeSpeed: 50, // Velocidad en mlisegundos para poner una letra,
+        startDelay: 200, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
         backSpeed: 75, // Velocidad en milisegundos para borrrar una letra,
         smartBackspace: true, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
         shuffle: false, // Alterar el orden en el que escribe las palabras.
